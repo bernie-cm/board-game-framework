cf8f0119dea4cc367f55fb82a0d40ec25eb658af
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2026-03-30 21:28:41 +1100

    Finish writing constructor

--- BoardGameFramework/Board.cs
+++ BoardGameFramework/Board.cs
@@ -1,10 +1,16 @@
+namespace BoardGameFramework;
+
 public abstract class Board {
   // Abstract class from whihc NTT, Gomoku and Notakto boards will inherit
   // This array will represent the board. Using string will accommodate different pieces of games, like "X", "O", or numbers
   protected string?[,] grid; 
-  public int Rows { get; set; };
-  public int Cols { get; set; };
+  public int Rows { get; }
+  public int Cols { get; }
 
-  public Board(int rows, int cols) {
+  // Since Board is abstract, no-one should be able to instantiate a Board directly so it's proetected
+  protected Board(int rows, int cols) {
+    Rows = rows;
+    Cols = cols;
+    grid = new string?[rows, cols];
   }
 }
