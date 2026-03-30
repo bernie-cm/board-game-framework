295131d17588bba5b128dddbc049409e98b18857
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2026-03-30 21:19:51 +1100

    Create data fields and constructor skeleton

--- BoardGameFramework/Board.cs
+++ BoardGameFramework/Board.cs
@@ -1,3 +1,10 @@
-public abstract class Player {
+public abstract class Board {
   // Abstract class from whihc NTT, Gomoku and Notakto boards will inherit
+  // This array will represent the board. Using string will accommodate different pieces of games, like "X", "O", or numbers
+  protected string?[,] grid; 
+  public int Rows { get; set; };
+  public int Cols { get; set; };
+
+  public Board(int rows, int cols) {
+  }
 }
