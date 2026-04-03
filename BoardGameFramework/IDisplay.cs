e1068b9934281e8c4ca8a881c84c6e65d3b291a0
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2026-04-03 21:12:03 +1100

    Write interface docstring for the IDisplay class

--- BoardGameFramework/IDisplay.cs
+++ BoardGameFramework/IDisplay.cs
@@ -1,6 +1,12 @@
 namespace BoardGameFramework;
 public interface IDisplay
 {
+    /*
+    The IDisplay interface defines the contract that defines every way the 
+    game framework talks to the user. For example, showing messages, showing the board
+    and getting input. For this framework, the implementation is defined separately in the
+    ConsoleDisplay.cs class
+    */
     void ShowBoard(Board board);
     void ShowMessage(string message);
     void ShowResult(string result);
