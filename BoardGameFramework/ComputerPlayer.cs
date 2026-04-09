f812eed8143ad357e80d1fc0c82a49941de23373
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2026-04-09 20:39:48 +1000

    Remove white lines and condense code

--- BoardGameFramework/ComputerPlayer.cs
+++ BoardGameFramework/ComputerPlayer.cs
@@ -1,20 +1,13 @@
 namespace BoardGameFramework.Core;
 
-public class ComputerPlayer : Player
-{
+public class ComputerPlayer : Player {
     // This private field stores the computer move logic interface
     private IComputerStrategy _strategy;
-    public ComputerPlayer(int playerNumber, string gamePiece, IComputerStrategy strategy) : base(playerNumber, gamePiece)
-    {
+    public ComputerPlayer(int playerNumber, string gamePiece, IComputerStrategy strategy) : base(playerNumber, gamePiece) {
         _strategy = strategy;
     }
-    public override (int row, int col, string value) MakeMove(Board board, IDisplay display)
-    {
-        /*
-        Calls strategy.ChooseMove(board, Piece). 
-        Displays what the computer chose via display.ShowMessage(). 
-        Returns the result
-        */
+    public override (int row, int col, string value) MakeMove(Board board, IDisplay display) {
+        // Calls strategy.ChooseMove(board, Piece), and Displays what the computer chose via display.ShowMessage(). 
         var computerMove = _strategy.ChooseMove(board, GamePiece);
         display.ShowMessage($"Computer Player has played {computerMove.value} at ({computerMove.row}, {computerMove.col}).");
         return computerMove;
